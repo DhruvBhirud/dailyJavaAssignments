15feac16834cd30c409ca70f660074a9f8a1f366
DhruvBhirud <bhiruddhruv@gmail.com>
2022-09-20 13:41:56 +0530

    Added findGreatestFromFive

--- 01.LEC.19.9.2022/findGreatestFromFive.java
+++ 01.LEC.19.9.2022/findGreatestFromFive.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
+//WAP to accept 5 no from user and find out the greatest
 public class findGreatestFromFive {
     public static void main (String args[]){
-        
+        int a,b,c,d,e;
+        System.out.println("Enter 5 Numbers");
+        Scanner sc= new Scanner(System.in);
+        a=sc.nextInt();
+        b=sc.nextInt();
+        c=sc.nextInt();
+        d=sc.nextInt();
+        e=sc.nextInt();
+        sc.close();
+        if ((a>b)&&(a>=c)&&(a>=d)&&(a>=e)) {
+            System.out.println("The Greatest number is : "+a);
+        }
+        else if ((b>=c)&&(b>=d)&&(b>=e)) {
+            System.out.println("The Greatest number is : "+b);
+        }
+        else if ((c>=d)&&(c>=e)) {
+            System.out.println("The Greatest number is : "+c);
+        }
+        else if ((d>=e)) {
+            System.out.println("The Greatest number is : "+d);
+        }
+        else{
+            System.out.println("The Greatest number is : "+e);
+        }
     }
 }
